6d06f44fc3dbec8335310626af7977ad88851b7d
Stefan Mehnert <stefanmehnert@gmail.com>
2015-01-03 06:52:24 +0100

    TC1 Okay, add 1 Item an verify List Size

--- Linked_List_V2.playground/section-1.swift
+++ Linked_List_V2.playground/section-1.swift
@@ -2,3 +2,36 @@
 
 import Cocoa
 
+class Element<T> {
+    var Data: T;
+    
+    init(data: T) {
+        Data = data;
+    }
+}
+
+class LinkedList<T> {
+    
+    private var size: Int = 0
+    
+    func Add<T>(item: T) {
+        self.size += 1
+    }
+    
+    func Delete(item: T) {
+        
+    }
+    
+    func Find<T>(item: T) {
+        
+    }
+    
+    func Size() -> Int {
+        return self.size;
+    }
+}
+
+// TC1: New list with 1 Item returns size 1
+let list = LinkedList<String>();
+list.Add("Hello World")
+assert(list.Size() == 1);
